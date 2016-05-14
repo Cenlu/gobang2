6e1d8256c17d9b1ae4c17168f7d0ce3e859c6821
pqy <QYPan@163.com>
2016-05-14 19:36:16 +0800

    new

--- computer.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMPUTER_H
-#define COMPUTER_H
-
-extern int test(int player);
-extern int computer_go(int player);
-
-#endif
--- run.bat
+++
@@ -1,1 +0,0 @@
-gcc -Wall -o a main.c face.c face.h fun.c fun.h computer.c computer.h
